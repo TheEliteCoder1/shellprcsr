35589bf5286497b9284f5977167e97c50b371cc9
PheNoMeNaL <74937838+Apple-Pies@users.noreply.github.com>
2021-04-16 09:46:08 -0400

    made a few minor updates

--- interpreter.py
+++ interpreter.py
@@ -13,13 +13,16 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 command_list = []
 defenition_list = []
 
+
 def add_command(c):
     return command_list.append(c)
+
 
 def add_def(d):
     return defenition_list.append(d)
 
 # adding commands to the command list
+
 
 '''
 System Types:
@@ -30,7 +33,7 @@ System Types:
 
 
 # All True Type System Variables
-ShellConfig = {"-a":"-a", "-h":"-h", "--file -a":"--file -a"}
+ShellConfig = {"-a": "-a", "-h": "-h", "--file -a": "--file -a"}
 
 # Commands List
 add_command(f"{ShellConfig['-a']} commands")
@@ -50,28 +53,34 @@ add_command("-h shout [text]")
 add_command("-h virt memory prcs -s")
 add_command("--file -a write [path]")
 add_command("--file -a read [path]")
+add_command("-h --foo-printer [amount] [input]")
 # functions to call
 add_command("help()")
 
 # adding defenitions to the defenitions list
 add_def(f"{ShellConfig['-a']} commands: shows all commands")
 add_def(f"{ShellConfig['-h']} txt [name] [folder]: creates a text file")
-add_def(f"{ShellConfig['--file -a']} make [name] [extension] [folder]: creates any type of file")
-add_def(f"{ShellConfig['-h']} list files: lists all of the files/directories from the current directory")
+add_def(
+    f"{ShellConfig['--file -a']} make [name] [extension] [folder]: creates any type of file")
+add_def(
+    f"{ShellConfig['-h']} list files: lists all of the files/directories from the current directory")
 add_def(f"{ShellConfig['-h']} info: gives a brief description about the shell")
-add_def(f"{ShellConfig['--file -a']} move [file path] [new file path]: moves an existing file to another directory")
+add_def(
+    f"{ShellConfig['--file -a']} move [file path] [new file path]: moves an existing file to another directory")
 add_def(f"{ShellConfig['-h']} quit shell -s: quits the shell")
 add_def(f"{ShellConfig['-h']} make dir [name]: creates a new directory")
-add_def(f"{ShellConfig['--file -a']} del [path]: deletes a file")
+add_def(f"{ShellConfig['--file -a']} del [path]:")
 add_def(f"{ShellConfig['-h']} del dir [path]: deletes a folder")
 add_def("help(): gives a description for each commands in the shell")
-add_def(f"{ShellConfig['-h']} cd dir [path]: changes current directory to specified path")
+add_def(
+    f"{ShellConfig['-h']} cd dir [path]: changes current directory to specified path")
 add_def("-h camera: opens webcam, press 'q' to quit session")
 add_def("-h chk disk usg: gives statistics on the usage of your system's disk")
 add_def("-h shout [text]: echos some text to the screen")
 add_def("-h virt memory prcs -s: gives statistics on your virtual memory's processes")
 add_def("--file -a write [path]: writes to a file")
 add_def("--file -a read [path]: reads a file")
+add_def("-h --foo-printer [amount] [input]: prints ascii value of foo input, and translates back to character based on foo input")
 
 '''
 Shell User Properties
@@ -79,10 +88,12 @@ Shell User Properties
 
 user_list = ["default"]
 
+
 def all_commands():
     for command in command_list:
-        #Shows a list of all the commands, sorted by number(least to greatest).
+        # Shows a list of all the commands, sorted by number(least to greatest).
         print(f"{command_list.index(command)}: {command}")
+
 
 def make_text_file(folder, name):
     if os.path.exists(folder):
@@ -93,6 +104,7 @@ def make_text_file(folder, name):
         file = open(f"{folder}/{name}.txt", "a")
         file.close()
 
+
 def make_file(folder, name, extension):
     if os.path.exists(folder):
         file = open(f"{folder}/{name}.{extension}", "a")
@@ -102,28 +114,34 @@ def make_file(folder, name, extension):
         file = open(f"{folder}/{name}.{extension}", "a")
         file.close()
 
+
 def current_directories():
-    for root, dirs, files in os.walk("."):
+    for root, dirs, files in os.walk("C:\\Users\\User\Documents"):
         for filename in files:
             print(Fore.CYAN + "  " + str(filename))
         for d in dirs:
             print(Fore.CYAN + "  " + str(d))
 
+
 def make_dir(new):
-    path = os.path.join(dir_path,new)
+    path = os.path.join(dir_path, new)
     os.mkdir(path)
     print(f"new directory '{new}' has been made.")
 
+
 def info():
     print(f"This is a command line interface. '#shellprcsr~' is for multi-purpose use.")
+
 
 def move_file(file_path, new_file_path):
     try:
         shutil.move(f"{file_path}", f"{new_file_path}")
         print(Fore.CYAN + "file has been moved successfully.")
     except:
-        print(Fore.YELLOW + f"'{file_path}' is an invalid directory, and or the new file path is invalid.")
-    
+        print(Fore.YELLOW +
+              f"'{file_path}' is an invalid directory, and or the new file path is invalid.")
+
+
 def del_file(file_path):
     if os.path.exists(file_path):
         shutil.rmtree(file_path)
@@ -132,6 +150,7 @@ def del_file(file_path):
     else:
         print(Fore.YELLOW + "This file path does not exist")
 
+
 def del_folder(path):
     if os.path.exists(path):
         os.remove(path)
@@ -139,18 +158,20 @@ def del_folder(path):
         print(Fore.CYAN + "folder successfully deleted (execution in 0.1 seconds)")
     else:
         print(Fore.YELLOW + "This folder path does not exist")
-    
+
 
 def call_help():
     for defenition in defenition_list:
-        #Shows all defenitions for commands, sorted by number(least to greatest).
+        # Shows all defenitions for commands, sorted by number(least to greatest).
         print(f"{defenition_list.index(defenition)}: {defenition}")
+
 
 def change_dir(path):
     if os.path.exists(path):
-        os.getcwd(path)
+        shutil.move(f"{dir_path}", f"{path}")
     else:
         print(Fore.YELLOW + "This directory does not exist")
+
 
 def write_file(path):
     x = 0
@@ -168,19 +189,23 @@ def write_file(path):
     else:
         print(Fore.YELLOW + "This path does not exist, please try again.")
 
+
 def read_file(path):
     width = os.get_terminal_size().columns
     print(Back.CYAN + Fore.BLACK + "File Reader".center(width))
     if os.path.exists(path):
         with open(path, "r") as file:
-            print(file.read())         
+            print(file.read())
     else:
         print(Fore.YELLOW + "This path does not exist, please try again.")
-        
+
+
 def camera():
     cap = cv2.VideoCapture(0)
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + 'haarcascade_eye.xml')
 
     while True:
         ret, frame = cap.read()
@@ -193,7 +218,8 @@ def camera():
             roi_color = frame[y:y+h, x:x+w]
             eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
             for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
+                cv2.rectangle(roi_color, (ex, ey),
+                              (ex + ew, ey + eh), (0, 255, 0), 5)
 
         cv2.imshow('frame', frame)
 
@@ -202,4 +228,3 @@ def camera():
 
     cap.release()
     cv2.destroyAllWindows()
- 
